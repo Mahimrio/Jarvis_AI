cacfa4e82afdb489c5229d8f0b206971293aec10
/ <mahim.cse.20230104015@aust.edu>
2026-08-15 18:11:14 +0600

    Ignore dist-electron build output

--- frontend/dist-electron/preload.mjs
+++
@@ -1,9 +0,0 @@
-let electron = require("electron");
-//#region electron/preload.ts
-electron.contextBridge.exposeInMainWorld("jarvis", {
-	desktop: true,
-	platform: process.platform,
-	quit: () => electron.ipcRenderer.invoke("jarvis:quit"),
-	minimize: () => electron.ipcRenderer.invoke("jarvis:minimize")
-});
-//#endregion
